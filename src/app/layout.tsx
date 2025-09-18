d8ff771388d2f4cdacebbcbf0130630cd9c17bbd
Abdul Majid <majidsheikh0096@gmail.com>
2025-09-19 02:01:26 +0500

    Add placeholder images and render StudentNavigation in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { StudentNavigation } from "@/components/unified-navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <StudentNavigation />
+        <div className="pt-16">{/* push content below fixed header */}
+          {children}
+        </div>
       </body>
     </html>
   );
